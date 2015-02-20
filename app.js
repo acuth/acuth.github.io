88606b66b12d57643a3b532feeb4afe008301d86
Adrian <>
2015-02-20 17:04:05 +0000

    now functionality actually works

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
-function onLoad() {
-  document.getElementById('footer').innerHTML = 'generated at '+(new Date());
+function doOnLoad() {
+  console.log('doOnLoad()');
+  document.getElementById('footer').innerHTML = 'Generated @ '+(new Date());
 }

302eedfdcc2f1a621aa33b15c85e19749c5b9134
Adrian <>
2015-03-07 10:22:11 +0000

    add confirm support

--- js/awac.js
+++ js/awac.js
@@ -1,124 +1,199 @@
 var _awac_;
 
+
+function MockContainer(awac) {
+  this.debug = true;
+  this.values = new Object();
+  this.varName = null;
+  this.awac = awac;
+}
+
+MockContainer.prototype.setVarName = function(varName) {
+  this.varName = varName;
+}
+
+MockContainer.prototype.toString = function() {
+  return "{MockContainer var:"+this.varName+"}";
+}
+
+MockContainer.prototype.setTitle = function(title) {
+  if (this.debug) console.log(this.varName+'.setTitle('+title+')');
+  document.title = title;
+}
+
+MockContainer.prototype.startPage = function() {
+  if (this.debug) console.log(this.varName+'.startPage()');
+}
+
+MockContainer.prototype.endPage = function() {
+  if (this.debug) console.log(this.varName+'.endPage()');
+  window.close();
+}
+
+MockContainer.prototype.unlockNavDrawer = function() {
+  if (this.debug) console.log(this.varName+'.unlockNavDrawer()');
+}
+
+MockContainer.prototype.set = function(name,value) {
+  if (this.debug) console.log(this.varName+'.set('+name+','+value+')');
+  this.values[name] = value;
+}
+
+MockContainer.prototype.get = function(name) {
+  var value = this.values[name];
+  if (this.debug) console.log(this.varName+'.get('+name+')='+value);
+  return value;
+}
+
+MockContainer.prototype.store = function(name,value) {
+  if (this.debug) console.log(this.varName+'.store('+name+','+value+')');
+  localStorage.setItem(name,value);
+}
+
+MockContainer.prototype.load = function(name) {
+  var value = localStorage.getItem(name);
+  if (this.debug) console.log(this.varName+'.load('+name+')='+value);
+  return value;
+}
+
+MockContainer.prototype.openPage = function(tag,url) {
+  if (this.debug) console.log(this.varName+'.openPage('+tag+','+url+')');
+  window.open(url,tag);
+}
+
+MockContainer.prototype.startPage = function() {
+  if (this.debug) console.log(this.varName+'.startPage()');
+}
+
+MockContainer.prototype.endPage = function() {
+  if (this.debug) console.log(this.varName+'.endPage()');
+  //window.close();
+}
+
+MockContainer.prototype.showDialog = function(msg,ok,cancel) {
+  if (this.debug) console.log(this.varName+'.showDialog('+msg+','+ok+','+cancel+')');
+  var yes = confirm(msg);
+  this.awac.doDialog(yes);
+}
+
+MockContainer.prototype.gotOnRefreshCB = function() {
+  if (this.debug) console.log(this.varName+'.gotOnRefreshCB()');
+}
+
+MockContainer.prototype.gotOnBackPressedCB = function() {
+  if (this.debug) console.log(this.varName+'.gotOnBackPressedCB()');
+}
+
+MockContainer.prototype.finishRefresh = function() {
+  if (this.debug) console.log(this.varName+'.finishRefresh()');
+}
+
+
 function Awac(varName) {
   this.varName = varName;
   console.log(this.varName+' = new Awac('+_awac_+')');
-  this.container = _awac_;
-  this.debug = true;
-  if (this.container) {
-    this.container.setVarName(varName);
-    this.debug = false;
-  }
+  this.container = _awac_ ? _awac_ : new MockContainer(this);
+  this.container.setVarName(varName);
+  this.ondialog = null;
   this.onrefresh = null;
   this.onresult = null;
-  this.loaded = false;
+  this.started = false;
+  this.debug = true;
 }
 
 Awac.prototype.toString = function() {
   return "{Awac var:"+this.varName+" loaded:"+this.loaded+" onresult:"+this.onresult+"}";
 }
 
-Awac.prototype.isRootPage = function() {
-  var root = this.container ? this.container.isRootPage() : false;
-  if (this.debug) console.log(this.varName+'.isRootPage()='+root);
-  return root;
-}
-
 Awac.prototype.setTitle = function(title) {
-  if (this.debug) console.log(this.varName+'.setTitle('+title+')');
-  if (this.container) this.container.setTitle(title);
+  this.container.setTitle(title);
 }
 
 Awac.prototype.unlockNavDrawer = function() {
-  if (this.debug) console.log(this.varName+'.unlockNavDrawer()');
-  if (this.container) this.container.unlockNavDrawer();
+  this.container.unlockNavDrawer();
 }
 
 Awac.prototype.set = function(name,value) {
-  if (this.debug) console.log(this.varName+'.set('+name+','+value+')');
-  if (this.container) this.container.set(name,value);
+  this.container.set(name,value);
 }
 
 Awac.prototype.get = function(name) {
-  var value = this.container ? this.container.get(name) : null;
-  if (this.debug) console.log(this.varName+'.get('+name+')='+value);
-  return value;
+  return this.container.get(name);
 }
 
 Awac.prototype.store = function(name,value) {
-  if (this.debug) console.log(this.varName+'.store('+name+','+value+')');
-  if (this.container) this.container.store(name,value);
+  this.container.store(name,value);
 }
 
 Awac.prototype.load = function(name) {
-  var value = this.container ? this.container.load(name) : null;
-  if (this.debug) console.log(this.varName+'.load('+name+')='+value);
-  return value;
+  return this.container.load(name);
 }
 
-Awac.prototype.showPage = function() {
-  if (this.debug) console.log(this.varName+'.showPage()');
-  if (!this.loaded) {
-    this.loaded = true;
-    if (this.container) this.container.pageLoaded();
+/* Call this to make the web page visible */
+Awac.prototype.startPage = function() {
+  if (!this.started) {
+    this.started = true;
+    this.container.startPage();
   }
 }
 
-Awac.prototype.newPage = function(name) {
-  if (this.debug) console.log(this.varName+'.newPage('+name+')');
-  if (this.container) this.container.newPage(name);
+/* End displaying the page and pop it off the stack */
+Awac.prototype.endPage = function() {
+  this.container.endPage();
 }
 
-Awac.prototype.finishPage = function() {
-  if (this.debug) console.log(this.varName+'.finishPage()');
-  if (this.container) this.container.finishPage();
+/* Pop a new page onto the stack */
+Awac.prototype.openPage = function(tag,url) {
+  this.container.openPage(tag,url);
 }
+
+Awac.prototype.dialog = function(msg,ok,cancel,cb) {
+  this.ondialog = cb;
+  this.container.showDialog(msg,ok,cancel);
+}
+
+// set callbacks
 
 Awac.prototype.setOnRefresh = function(cb) {
-  if (this.debug) console.log(this.varName+'.setOnRefresh()');
-  if (this.container) this.container.gotOnRefreshCB();
+  this.container.gotOnRefreshCB();
   this.onrefresh = cb;
 }
 
-Awac.prototype.doRefresh = function() {
-  if (this.debug) console.log(this.varName+'.doRefresh()');
-  if (this.onrefresh) this.onrefresh();
-}
-
 Awac.prototype.setOnResult = function(cb) {
-  if (this.debug) console.log(this.varName+'.setOnResult()');
-  if (this.container) this.container.gotOnResultCB();
+  this.container.gotOnResultCB();
   this.onresult = cb;
 }
 
-Awac.prototype.doResult = function(ok) {
-  if (this.debug) console.log(this.varName+'.doResult('+ok+')');
-  if (this.onresult) this.onresult(ok);
-}
-
 Awac.prototype.setOnBackPressed = function(cb) {
-  if (this.debug) console.log(this.varName+'.setOnBackPressed()');
-  if (this.container) this.container.gotOnBackPressedCB();
+  this.container.gotOnBackPressedCB();
   this.onbackpressed = cb;
 }
 
+Awac.prototype.doRefresh = function() {
+  if (this.debug) console.log('Awac.doRefresh()');
+  if (this.onrefresh) this.onrefresh();
+}
+
+// fire callbacks
+
+Awac.prototype.doDialog = function(ok) {
+  if (this.debug) console.log('Awac.doDialog('+ok+')');
+  if (this.ondialog) this.ondialog(ok);
+  this.ondialog = null;
+}
+
+Awac.prototype.doResult = function(ok) {
+  if (this.debug) console.log('Awac.doResult('+ok+')');
+  if (this.onresult) this.onresult(ok);
+}
+
 Awac.prototype.doBackPressed = function() {
-  if (this.debug) console.log(this.varName+'.doBackPressed()');
+  if (this.debug) console.log('Awac.doBackPressed()');
   if (this.onbackpressed) this.onbackpressed();
 }
 
 Awac.prototype.finishRefresh = function() {
-  if (this.debug) console.log(this.varName+'.finishRefresh()');
+  if (this.debug) console.log('Awac.finishRefresh()');
   if (this.container) this.container.finishRefresh();
 }
 
-Awac.prototype.loading = function(msg) {
-  if (this.debug) console.log(this.varName+'.loading('+msg+')');
-  if (!msg) msg = ' ';
-  if (this.container) this.container.loading(msg);
-}
-
-Awac.prototype.toast = function(msg) {
-  if (this.debug) console.log(this.varName+'.toast('+msg+')');
-  if (this.container) this.container.showToast(msg);
-}

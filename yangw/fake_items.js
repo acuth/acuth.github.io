ae5f8cb31cdaa04745ce470a4b49ab62ef670aac
Adrian Cuthbert <adrian.cuthbert@gmail.com>
2015-12-05 17:30:01 +0000

    get Item from yangw-2.appspot.com

--- yangw/fake_items.js
+++ yangw/fake_items.js
@@ -16,31 +16,67 @@ function ajax(url,cb) {
 	xhr.send();
 }
 
-function FItem(name,json,markdown) {
-  this.name = name;
+
+function AttrLink() {
+  this.imageUrl = null;
+  this.attrType = null;
+  this.left = null;
+  this.right = null;
+}
+
+AttrLink.prototype.setAttrType=function(attrType) {
+  this.attrType = attrType;
+  return this;
+};
+
+AttrLink.prototype.setImageURL=function(imageUrl) {
+  this.imageUrl = imageUrl;
+  return this;
+};
+
+AttrLink.prototype.setLeft=function(label,action) {
+  this.left = {};
+  this.left.label = label;
+  this.left.action = action;
+  return this;
+};
+
+AttrLink.prototype.setRight=function(label,action) {
+  this.right = {};
+  this.right.label = label;
+  this.right.action = action;
+  return this;
+};
+
+AttrLink.prototype.render=function() {
+  var s = '<span class="attr-link';
+  if (this.attrType) s += ' '+this.attrType;
+  s += '">';
+  if (this.imageUrl) s+= '<img src="'+this.imageUrl+'" />';
+  if (this.left) s += '<a class="left" href="javascript:'+this.left.action+';">'+this.left.label+'</a>';
+  if (this.right) s += '<a class="right" href="javascript:'+this.right.action+';">'+this.right.label+'</a>';
+  s += '</span>';
+  return s;
+};
+
+
+
+function FItem(json) {
   this.json = JSON.parse(json);
-  if (markdown) markdown = markdown.trim();
-  this.markdown = markdown;
 }
 
 FItem.prototype.toString=function() {
-  return '{FItem name='+this.name+' json='+JSON.stringify(this.json)+' markdown='+this.markdown+'}';
+  return '{FItem name='+this.json.wiki_name+' json='+JSON.stringify(this.json)+'}';
 };
 
 var _fItemMap = {};
 
 FItem.get=function(name,cb) {
-  var fitem = _fItemMap[name];
-  if (fitem) 
+  ajax('https://yangw-2.appspot.com/v4/?op=get_item&wiki_name='+name,function(json) {
+    console.log('got '+json);
+    fitem = new FItem(json);
     cb(fitem);
-  else
-    ajax(name+'.json',function(json) {
-      ajax(name+'.md',function(markdown) {
-        fitem = new FItem(name,json,markdown);
-        _fItemMap[name] = fitem;
-        cb(fitem);
-      });
-    });
+  });
 };
 
 FItem.historyPush=function(awac,name){
@@ -73,19 +109,56 @@ FItem.prototype.addLink=function(html,pageName,pageTitle) {
   if (i != -1) html = html.replace(link,'<a href="javascript:showNextPage(\''+pageName+'\');">'+pageTitle+'</a>');
   return html;
 };
+
+FItem.prototype.getUserHTML=function(user,readOnly) {
+  if (!user) return '';
+  var link = (new AttrLink()).setAttrType('user-link').setImageURL(user.image_url).setLeft(user.name_attr,'showNextPage(\''+user.wiki_name+'\')');
+  if (!readOnly) link.setRight('+','showOptions(\'user\')');
+  return link.render();
+};
   
 FItem.prototype.addLinks=function(html) {
   if (!html) return null;
-  for (var i=0;i<this.json.links.length;i++) {
-    var link = this.json.links[i];
-    html = this.addLink(html,link.name,link.title);  
+  var i = 0;
+  var j = 0;
+  for (var k=0;k<20;k++) {
+    i = this.json.markdown.indexOf('[[',j);
+    if (i == -1) break;
+    j = this.json.markdown.indexOf(']]',i); 
+    if (j == -1) break;
+    var target = this.json.markdown.substring(i+2,j);
+    var name = target.substring(0,target.indexOf(':'));
+    var inline = this.json.markdown_inline_values[target];
+    if (name == 'wiki') {
+      html = html.replace('[['+target+']]','<a class="internal-link" href="javascript:showNextPage(\''+inline.wiki_name+'\');">'+inline.name_attr+'</a>');
+    }
+    else if (name == 'http' || name == 'https') {
+      html = html.replace('[['+target+']]','<a target="_blank" class="external-link" href="'+target+'">'+target+'</a>');
+    }
+    else if (name == 'tag') {
+      var a = (new AttrLink()).setLeft(inline.name_attr,'showNextPage(\''+inline.wiki_name+'\')').setRight('+','showOptions(\'tag\')').render();
+      html = html.replace('[['+target+']]',a);
+    }
+    else if (name == 'user') {
+      html = html.replace('[['+target+']]',this.getUserHTML(inline,false));
+    }
+    if (inline) {
+      console.log(target+'='+JSON.stringify(inline));
+    }
+    i = j;
   }
   return html;
 };
 
 FItem.prototype.getHTML=function() {
-   var converter = new showdown.Converter();
-   var html = converter.makeHtml(this.markdown);
+   var renderer = new marked.Renderer();
+   // do not convert links
+   renderer.link = function(href, title, text) { return href; };
+   var html = marked(this.json.markdown,{ renderer: renderer });
+   //console.log('using marked\n\n'+html);
+   //var converter = new showdown.Converter();
+   //html = converter.makeHtml(this.json.markdown);
+   //console.log('using showdown\n\n'+html);
    html = this.addLinks(html);
    return html;
 };
